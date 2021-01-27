40908c1173b435d7517e57b6a4e811d4722bebdc
inksword <jeff.jianfeng.jin@gmail.com>
2021-01-27 21:06:40 +0800

    apply DAG and children mock reference

--- scripts/shell/sfdx.sh
+++ scripts/shell/sfdx.sh
@@ -1,4 +1,5 @@
-sfdx force:package:version:create -p ApexTestKit -x -c --wait 10
+sfdx force:source:push
+sfdx force:package:version:create -p ApexTestKit -x -c --wait 10 --codecoverage
 sfdx force:package:version:list
 sfdx force:package:version:promote -p 04t2v000007GQuwAAG
 sfdx force:package:version:report -p 04t2v000007GQuwAAG
